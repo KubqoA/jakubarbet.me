6f685c8dd6503c77be3e80d7a2412d8b3678244e
Jakub Arbet <hi@jakubarbet.me>
2024-04-22 02:45:44 +0200

    Render markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import markdownIntegration from "@astropub/md";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     domains: ["drive.jakubarbet.me"],
   },
-  integrations: [tailwind()]
+  integrations: [tailwind(), markdownIntegration(),]
 });
