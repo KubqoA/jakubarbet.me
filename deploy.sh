5c6a414f4f3c29c6b09a9960f401bb2b1ce67ecf
Jakub Arbet <hi@jakubarbet.me>
2024-05-06 13:16:53 +0200

    Switch to bun, add deploy action

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-pnpm build
+bun run build
 tar -czvf new.tar.gz dist
-echo "put new.tar.gz ./jakubarbet.me/" | sftp jakub@organ.jakubarbet.me
-echo "rm -Rf ~/jakubarbet.me/dist && tar -xzvf ~/jakubarbet.me/new.tar.gz -C ~/jakubarbet.me && rm -Rf ~/jakubarbet.me/public_html && mv ~/jakubarbet.me/{dist,public_html} && rm ~/jakubarbet.me/new.tar.gz" | ssh jakub@organ.jakubarbet.me
+echo "put new.tar.gz ./jakubarbet.me/" | sftp jakub@jakubarbet.me
+echo "rm -Rf ~/jakubarbet.me/dist && tar -xzvf ~/jakubarbet.me/new.tar.gz -C ~/jakubarbet.me && rm -Rf ~/jakubarbet.me/public_html && mv ~/jakubarbet.me/{dist,public_html} && rm ~/jakubarbet.me/new.tar.gz" | ssh jakub@jakubarbet.me
 rm new.tar.gz
 echo "Deployed new version"
 
