cb33972411694c346abfb4450beef9eb306ab8ff
Jakub Arbet <hi@jakubarbet.me>
2025-07-21 10:45:32 +0000

    Start fresh with Astro 5.0

--- src/utils/galleries.ts
+++
@@ -1,6 +0,0 @@
-export const getGalleryImages = (slug: string) =>
-  Promise.all(
-    Object.entries(import.meta.glob<{default: ImageMetadata}>('/src/content/galleries/**/*.{jpeg,jpg,png,webp}'))
-      .filter(([key, _]) => key.includes(slug))
-      .map(async ([_, image]) => (await image()).default),
-  )
